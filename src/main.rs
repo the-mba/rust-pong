ee3e363651102e4d936cf5098ecba8e7c2e64de6
Simeone_Ibiza <sirmariothegentleman@gmail.com>
2023-12-31 18:55:05 +0100

    --- Auto Git Commit ---

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,6 @@ use bevy::{
     sprite::collide_aabb::{collide, Collision},
     sprite::MaterialMesh2dBundle,
 };
-use std::iter::zip;
 
 fn main() {
     let parameters = parameters_from_toml();
@@ -20,7 +19,7 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_plugins(FrameTimeDiagnosticsPlugin)
         .add_state::<AppStates>()
-        .add_systems(OnEnter(AppState::Menu), setup_menu)
+        .add_systems(OnEnter(AppStates::Menu), menu::setup_menu)
         .insert_resource(Scoreboard {
             scores: vec![0; parameters.players.len()],
         })
@@ -28,6 +27,16 @@ fn main() {
         .insert_resource(Speed(parameters.ball.speed))
         .insert_resource(ClearColor(parameters.colors.background))
         .add_event::<CollisionEvent>()
+        .add_systems(OnEnter(AppState::Menu), menu::setup_menu)
+        // By contrast, update systems are stored in the `Update` schedule. They simply
+        // check the value of the `State<T>` resource to see if they should run each frame.
+        .add_systems(Update, menu::run_menu.run_if(in_state(AppState::Menu)))
+        .add_systems(OnExit(AppState::Menu), cleanup_menu)
+        .add_systems(OnEnter(AppState::InGame), setup_game)
+        .add_systems(
+            Update,
+            (movement, change_color).run_if(in_state(AppState::InGame)),
+        )
         .add_systems(Startup, setup)
         // Add our gameplay simulation systems to the fixed timestep schedule
         // which runs at 64 Hz by default
@@ -45,6 +54,142 @@ fn main() {
         )
         .add_systems(Update, (update_scoreboards, bevy::window::close_on_esc))
         .run();
+}
+
+mod menu {
+    use bevy::prelude::*;
+
+    #[derive(Debug, Clone, Copy, Default, Eq, PartialEq, Hash, States)]
+    enum AppState {
+        #[default]
+        Menu,
+        InGame,
+    }
+
+    #[derive(Resource)]
+    struct MenuData {
+        button_entity: Entity,
+    }
+
+    const NORMAL_BUTTON: Color = Color::rgb(0.15, 0.15, 0.15);
+    const HOVERED_BUTTON: Color = Color::rgb(0.25, 0.25, 0.25);
+    const PRESSED_BUTTON: Color = Color::rgb(0.35, 0.75, 0.35);
+
+    fn setup(mut commands: Commands) {
+        commands.spawn(Camera2dBundle::default());
+    }
+
+    fn setup_menu(mut commands: Commands) {
+        let button_entity = commands
+            .spawn(NodeBundle {
+                style: Style {
+                    // center button
+                    width: Val::Percent(100.),
+                    height: Val::Percent(100.),
+                    justify_content: JustifyContent::Center,
+                    align_items: AlignItems::Center,
+                    ..default()
+                },
+                ..default()
+            })
+            .with_children(|parent| {
+                parent
+                    .spawn(ButtonBundle {
+                        style: Style {
+                            width: Val::Px(150.),
+                            height: Val::Px(65.),
+                            // horizontally center child text
+                            justify_content: JustifyContent::Center,
+                            // vertically center child text
+                            align_items: AlignItems::Center,
+                            ..default()
+                        },
+                        background_color: NORMAL_BUTTON.into(),
+                        ..default()
+                    })
+                    .with_children(|parent| {
+                        parent.spawn(TextBundle::from_section(
+                            "Play",
+                            TextStyle {
+                                font_size: 40.0,
+                                color: Color::rgb(0.9, 0.9, 0.9),
+                                ..default()
+                            },
+                        ));
+                    });
+            })
+            .id();
+        commands.insert_resource(MenuData { button_entity });
+    }
+
+    fn menu(
+        mut next_state: ResMut<NextState<AppState>>,
+        mut interaction_query: Query<
+            (&Interaction, &mut BackgroundColor),
+            (Changed<Interaction>, With<Button>),
+        >,
+    ) {
+        for (interaction, mut color) in &mut interaction_query {
+            match *interaction {
+                Interaction::Pressed => {
+                    *color = PRESSED_BUTTON.into();
+                    next_state.set(AppState::InGame);
+                }
+                Interaction::Hovered => {
+                    *color = HOVERED_BUTTON.into();
+                }
+                Interaction::None => {
+                    *color = NORMAL_BUTTON.into();
+                }
+            }
+        }
+    }
+
+    fn cleanup_menu(mut commands: Commands, menu_data: Res<MenuData>) {
+        commands.entity(menu_data.button_entity).despawn_recursive();
+    }
+
+    fn setup_game(mut commands: Commands, asset_server: Res<AssetServer>) {
+        commands.spawn(SpriteBundle {
+            texture: asset_server.load("branding/icon.png"),
+            ..default()
+        });
+    }
+
+    const SPEED: f32 = 100.0;
+    fn movement(
+        time: Res<Time>,
+        input: Res<ButtonInput<KeyCode>>,
+        mut query: Query<&mut Transform, With<Sprite>>,
+    ) {
+        for mut transform in &mut query {
+            let mut direction = Vec3::ZERO;
+            if input.pressed(KeyCode::ArrowLeft) {
+                direction.x -= 1.0;
+            }
+            if input.pressed(KeyCode::ArrowRight) {
+                direction.x += 1.0;
+            }
+            if input.pressed(KeyCode::ArrowUp) {
+                direction.y += 1.0;
+            }
+            if input.pressed(KeyCode::ArrowDown) {
+                direction.y -= 1.0;
+            }
+
+            if direction != Vec3::ZERO {
+                transform.translation += direction.normalize() * SPEED * time.delta_seconds();
+            }
+        }
+    }
+
+    fn change_color(time: Res<Time>, mut query: Query<&mut Sprite>) {
+        for mut sprite in &mut query {
+            sprite
+                .color
+                .set_b((time.elapsed_seconds() * 0.5).sin() + 2.0);
+        }
+    }
 }
 
 // Add the game's entities to our world
